ff177062000e66909ae63a2b65276771a6e504e0
Hiroyuki Suzuki <suzuki.hiroyuki@mbr.nifty.com>
2022-04-30 18:45:43 +0900

    カウンタプログラム作成

---
+++ myCounter/ViewModels/CountDownCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace myCounter.ViewModels
+{
+    public class CountDownCommand: ICommand
+    {
+        private CounterViewModel _view { get; set; }
+
+        public CountDownCommand(CounterViewModel view)
+        {
+            _view = view;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; } 
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _view.Value = (_view.Value <= 0) ? 100 : _view.Value - 1;
+        }
+    }
+}
---
+++ myCounter/ViewModels/CounterViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using myCounter.Models;
+
+namespace myCounter.ViewModels
+{
+    public class CounterViewModel
+    {
+        public event PropertyChangedEventHandler propertyChanged;
+
+        public Counter counter { get; set; }
+        public CountDownCommand countDownCommand { get; private set; }
+
+        public CounterViewModel()
+        {
+            countDownCommand = new CountDownCommand(this);
+            counter = new Counter();
+        }
+
+        public int Value
+        {
+            get
+            {
+                return counter.Value;
+            }
+            set
+            {
+                counter.Value = value;
+
+                if (propertyChanged != null)
+                {
+                    propertyChanged(this, new PropertyChangedEventArgs("Value"));
+                }
+            }
+        }
+    }
+}
